2fe65c3dd7190ab0199d1943c96f252cefdd79a9
Ryder <ryderjpm@gmail.com>
2026-01-04 19:27:00 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (14.80%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        result = [False] * len(candies)
+        mostCandies = max(candies)
+        
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= mostCandies:
+                result[i] = True
+        
+        return result
