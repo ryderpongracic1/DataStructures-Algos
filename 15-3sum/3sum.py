d67ebb4ad2b17ba5b8da77cd0f6fe9f2c0c62e98
Ryder <ryderjpm@gmail.com>
2026-01-13 11:52:03 -0500

    Time: 708 ms (70.86%) | Memory: 19.1 MB (11.75%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -27,8 +27,5 @@ class Solution(object):
                     
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
-                        
-                    while left < right and nums[right] == nums[right+1]:
-                        right -= 1
         
         return ans
