bbfdb3a8f4e71d0fc1b1a382541ee29fcc63bc38
Ryder <ryderjpm@gmail.com>
2026-03-04 00:15:21 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (81.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,9 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
-        dp = [0] * n # dp[i] = number of ways to get to step i
-        dp[0] = 1
-        dp[1] = 2
+        prev2 = 1
+        prev1 = 2
         for i in range(2, n):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n - 1]
+            temp = prev1 + prev2
+            prev2 = prev1
+            prev1 = temp
+        return prev1
