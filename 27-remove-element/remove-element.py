4e8ed924e04343f7a7883d29a71f37d2981cb8ff
Ryder <ryderjpm@gmail.com>
2026-04-08 19:32:16 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.93%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        writer = 0
-        for reader in range(len(nums)):
-            if nums[reader] != val:
-                nums[writer] = nums[reader]
-                writer += 1
-        return writer
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        count, left = 0, n - 1
+
+        for right in range(n - 1, -1, -1):
+            if nums[right] == val:
+                nums[left], nums[right] = nums[right], nums[left]
+                left -= 1
+        
+        return left + 1
