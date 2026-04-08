e228d4f83775dded204820a66a03ac7c84073708
Ryder <ryderjpm@gmail.com>
2026-04-08 19:24:08 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (84.18%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -6,19 +6,11 @@ class Solution:
         end = m + n - 1
         p1, p2 = m - 1, n - 1
 
-        while end > -1:
-            if p1 >= 0 and p2 >= 0:
-                if nums1[p1] > nums2[p2]:
-                    nums1[end] = nums1[p1]
-                    p1 -= 1
-                else:
-                    nums1[end] = nums2[p2]
-                    p2 -= 1
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[end] = nums1[p1]
+                p1 -= 1
             else:
-                if p1 >= 0:
-                    nums1[end] = nums1[p1]
-                    p1 -= 1
-                else:
-                    nums1[end] = nums2[p2]
-                    p2 -= 1
+                nums1[end] = nums2[p2]
+                p2 -= 1
             end -= 1
