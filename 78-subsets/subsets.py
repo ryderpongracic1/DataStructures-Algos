e48dd3591df939ed9ddb864cf7d1e1a08fe01e1c
ryder <ryderjpm@gmail.com>
2026-05-21 17:12:20 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (67.03%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,11 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        def backtrack(idx, subset):
-            ans.append(subset[:])
-            for i in range(idx, len(nums)):
-                subset.append(nums[i])
-                backtrack(i + 1, subset)
-                subset.pop()
-        backtrack(0, [])
-        return ans
+        res = []
+
+        # explore decision tree
+        def dfs(subset, idx):
+            if idx == len(nums): # reached decision tree leaf node
+                res.append(subset[:])
+            elif idx < len(nums):
+                dfs(subset, idx + 1) # exclude current num & explore
+                subset.append(nums[idx]) # include current num
+                dfs(subset, idx + 1) # explore inclusion branch
+                subset.pop() # backtrack
+        dfs([], 0)
+        return res
