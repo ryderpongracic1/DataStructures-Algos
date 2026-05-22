4f99240d7faafaa45c2bff381341195045a38ab9
ryder <ryderjpm@gmail.com>
2026-05-22 11:43:15 -0400

    Time: 3 ms (95.41%) | Memory: 19.7 MB (9.15%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -4,22 +4,15 @@ class Solution:
         res = []
 
         def dfs(curr, total, idx):
-            # base case 1: answer
             if total == target:
                 res.append(curr[:])
                 return
-
-            # each candidate tried once per recursion level
             for i in range(idx, len(candidates)):
-                # prune impossbile answers (sorted nums)
                 if total + candidates[i] > target:
                     break
                 curr.append(candidates[i])
-                # passing i (not i + 1) lets reuse in deeper recursion levels
                 dfs(curr, total + candidates[i], i)
-
-                # restores curr so next i can explore diff branch
                 curr.pop()
-        
+
         dfs([], 0, 0)
         return res
