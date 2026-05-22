c147793102fb426394b19555e7ade93b5fb3a99e
ryder <ryderjpm@gmail.com>
2026-05-22 11:39:52 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (67.03%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,12 +2,14 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        # explore decision tree
         def dfs(subset, idx):
+            # choose current valid subset
             res.append(subset[:])
+            # try every num once per recursion level
             for i in range(idx, len(nums)):
-                subset.append(nums[i]) # include current num
-                dfs(subset, i + 1) # explore branch
+                subset.append(nums[i]) # include i
+                dfs(subset, i + 1) # explore path
                 subset.pop() # backtrack
+
         dfs([], 0)
         return res
