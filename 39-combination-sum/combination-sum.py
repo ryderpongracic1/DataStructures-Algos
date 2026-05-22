0afe1cf1bfb2d02e9adaee41cd52c2b6abc6767e
ryder <ryderjpm@gmail.com>
2026-05-21 20:27:28 -0400

    Time: 2 ms (96.66%) | Memory: 19.6 MB (33.21%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
         res = []
 
         def dfs(curr, total, idx):
@@ -8,14 +9,12 @@ class Solution:
                 res.append(curr[:])
                 return
 
-            # base case 2: overshot target
-            if total > target:
-                return
-
             # each candidate tried once per recursion level
             for i in range(idx, len(candidates)):
+                # prune impossbile answers (sorted nums)
+                if total + candidates[i] > target:
+                    break
                 curr.append(candidates[i])
-
                 # passing i (not i + 1) lets reuse in deeper recursion levels
                 dfs(curr, total + candidates[i], i)
 
