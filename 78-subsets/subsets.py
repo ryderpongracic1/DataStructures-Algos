a011dcbd118ef092f48ed687a6dfe080e016ba49
ryder <ryderjpm@gmail.com>
2026-05-26 11:15:56 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.97%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -3,13 +3,11 @@ class Solution:
         res = []
 
         def dfs(subset, idx):
-            # choose current valid subset
             res.append(subset[:])
-            # try every num once per recursion level
             for i in range(idx, len(nums)):
-                subset.append(nums[i]) # include i
-                dfs(subset, i + 1) # explore path
-                subset.pop() # backtrack
+                subset.append(nums[i])
+                dfs(subset, i + 1)
+                subset.pop()
 
         dfs([], 0)
         return res
