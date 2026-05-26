38b5077610b5e6f3455bd8df4277b15751616418
ryder <ryderjpm@gmail.com>
2026-05-26 16:01:57 -0400

    Time: 7 ms (78.20%) | Memory: 12.5 MB (7.31%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,3 +1,9 @@
+# - each element may be chosen at most 1x per combination
+# - solution set contains no duplicate combinations
+# - sort to have duplicates adjacent, faster pruning
+# - dfs backtracking: 
+#    - each element considered once per recursion level
+#    - no reuse of elements (pass i + 1)
 class Solution(object):
     def combinationSum2(self, candidates, target):
         """
@@ -5,19 +11,23 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        ans = []
         candidates.sort()
-        def backtrack(idx, curr, total):
+        res = []
+
+        def dfs(curr, total, idx):
             if total == target:
-                ans.append(curr[:])
+                res.append(curr[:])
                 return
-            if total > target:
-                return
+            # for loop explores element for current slot in combination
             for i in range(idx, len(candidates)):
+                # branch is a dead end
+                if total + candidates[i] > target:
+                    break
+                # no reuse duplicate for CURRENT slot in current combination
                 if i > idx and candidates[i] == candidates[i - 1]:
                     continue
                 curr.append(candidates[i])
-                backtrack(i + 1, curr, total + candidates[i])
+                dfs(curr, total + candidates[i], i + 1)
                 curr.pop()
-        backtrack(0, [], 0)
-        return ans
+        dfs([], 0, 0)
+        return res
