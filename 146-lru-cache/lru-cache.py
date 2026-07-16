c932cfefd7e70c377a92a962a7f7121cf0188d62
ryder <ryderjpm@gmail.com>
2026-07-15 21:23:28 -0400

    Time: 122 ms (57.11%) | Memory: 78.7 MB (25.82%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,34 +1,26 @@
-# DLL Node
 class Node:
     def __init__(self, key=0, val=0):
         self.key, self.val = key, val
-        self.next = None
-        self.prev = None
-
+        self.next, self.prev = None, None
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {} # key -> node
         self.size = capacity
-
-        # dummy head & tail for MRU & LRU access
+        self.cache = {}
         self.head = Node()
-        self.tail = Node
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    # HELPER methods
-    # insert node into DLL
     def insert(self, node):
         prev_head = self.head.next
-        self.head.next = node
-        node.prev = self.head
-        node.next = prev_head
         prev_head.prev = node
-    # remove node from DLL
+        node.next = prev_head
+        node.prev = self.head
+        self.head.next = node
+
     def remove(self, node):
-        next_ptr = node.next
-        prev_ptr = node.prev
+        next_ptr, prev_ptr = node.next, node.prev
         next_ptr.prev = prev_ptr
         prev_ptr.next = next_ptr
 
@@ -44,12 +36,12 @@ class LRUCache:
         if key in self.cache:
             node = self.cache[key]
             self.remove(node)
-            self.insert(node)
             node.val = value
+            self.insert(node)
         else:
             node = Node(key, value)
-            self.cache[key] = node
             self.insert(node)
+            self.cache[key] = node
             if len(self.cache) > self.size:
                 lru = self.tail.prev
                 self.remove(lru)
