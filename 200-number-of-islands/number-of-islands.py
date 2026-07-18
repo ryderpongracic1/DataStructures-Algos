7a4447dd491bbb240eaf224f0946bb27a7f6c25d
ryder <ryderjpm@gmail.com>
2026-07-17 20:08:40 -0400

    Time: 235 ms (81.74%) | Memory: 21.5 MB (72.05%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,25 +1,26 @@
+'''
+Loop through grid -> find 1 -> dfs on 1, switching each cell to 0
+'''
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         count = 0
-        visited = set()
 
         def dfs(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == '0':
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS:
                 return
-            if (r, c) in visited:
+            if grid[r][c] != '1':
                 return
-            visited.add((r, c))
+            # grid[r][c] = 1
+            grid[r][c] = 0
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
             dfs(r, c - 1)
-
+            return
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == '1':
-                    if (r, c) in visited:
-                        continue
-                    dfs(r, c)
                     count += 1
+                    dfs(r, c)
         return count
