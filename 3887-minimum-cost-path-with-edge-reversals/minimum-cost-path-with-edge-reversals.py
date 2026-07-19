0707a0785b3879a93b8a3fc8bb7d166e01be0778
ryder <ryderjpm@gmail.com>
2026-07-18 20:42:11 -0400

    Time: 578 ms (38.56%) | Memory: 75.1 MB (24.16%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -0,0 +1,24 @@
+import heapq
+class Solution:
+    def minCost(self, n: int, edges: List[List[int]]) -> int:
+        adj = collections.defaultdict(list)
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            adj[v].append((u, 2 * w))
+
+        min_costs = {i: float('inf') for i in range(n)}
+        heap = [(0, 0)]
+
+        while heap:
+            cost, node = heapq.heappop(heap)
+            if cost > min_costs[node]:
+                continue
+            min_costs[node] = cost
+            if node == n - 1:
+                return cost
+            for v, w in adj[node]:
+                curr_cost = cost + w
+                if curr_cost < min_costs[v]:
+                    min_costs[v] = curr_cost
+                    heapq.heappush(heap, (curr_cost, v))
+        return -1
