b8d1c7191e97ed69792153b051aea784ff3cb795
ryder <ryderjpm@gmail.com>
2026-07-19 00:51:43 -0400

    Time: 4 ms (92.73%) | Memory: 22.6 MB (92.85%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,26 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-import heapq
+'''
+- Look at 2 lists at a time - merge them like mergeTwoLinkedLists
+- Repeat until only 1 list remains
+'''
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        minHeap = []
-        i = 0
-        for LL in lists:
-            if LL:
-                minHeap.append((LL.val, i, LL))
-                i += 1
-        heapq.heapify(minHeap)
+        if not lists:
+            return None
+        while len(lists) > 1:
+            merged = []
 
-        dummy = ListNode()
-        curr = dummy
+            # look at 2 lists at a time
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                if i + 1 >= len(lists):
+                    l2 = None
+                else:
+                    l2 = lists[i + 1]
+                merged.append(self.mergeTwoLists(l1, l2))
+            lists = merged
+        return lists[0]
 
-        while minHeap:
-            val, idx, node = heapq.heappop(minHeap)
-            if node.next:
-                i += 1
-                heapq.heappush(minHeap, (node.next.val, i, node.next))
-            curr.next = node
+    def mergeTwoLists(self, list1, list2):
+        head = ListNode(0)
+        curr = head
+        while list1 and list2:
+            if list1.val > list2.val:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
             curr = curr.next
-
-        return dummy.next
+        if list1:
+            curr.next = list1
+        elif list2:
+            curr.next = list2
+        return head.next
