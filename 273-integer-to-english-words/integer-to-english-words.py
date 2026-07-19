1c5ed80c74181119d1f189ade2d3879232713c04
ryder <ryderjpm@gmail.com>
2026-07-19 19:21:51 -0400

    Time: 0 ms (100.00%) | Memory: 19.7 MB (43.12%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -3,38 +3,50 @@ class Solution:
         if num == 0:
             return 'Zero'
 
-        # Look-up tables for irregular numbers
-        below_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
-            "Eight", "Nine","Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
-            "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-
-        tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
-            "Seventy", "Eighty", "Ninety"]
-
+        # lookup tables
+        below_20 = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six',
+            'Seven','Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen',
+            'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen',
+            'Nineteen']
+        tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty',
+            'Seventy', 'Eighty', 'Ninety']
         scale = ['', 'Thousand', 'Million', 'Billion']
 
+        # Convert n (0-999) to words
         def helper(n):
+            # zero is blank in words
             if n == 0:
                 return ''
+
+            # n is 1-19
+            # always outputs num + space for potential scalar
+            # eg 'One ' + 'Thousand'
             elif n < 20:
                 return below_20[n] + ' '
-            elif n < 100:
-                # peels tens digit & recurses on remainder
-                return tens[n // 10] + ' ' + helper(n % 10)
-            else:
-                # peels hundred digit & recurses on 2-digit remainder
-                return below_20[n // 100] + ' Hundred ' + helper(n % 100)
 
+            # n is 20 - 99
+            elif n < 100:
+                #      tens amount   + ones amount
+                # ex: 23: 23 // 10 = 2, 23 % 10 = 3
+                # tens[2] = 'Twenty'
+                # helper(3) = 'Three '
+                return tens[n // 10] + ' ' + helper(n % 10)
+
+            # n is 100-999
+            else:
+                # n / 100: strip 3 digit num to 1 digit
+                # n % 100: get 2 rightmost digits
+                return below_20[n // 100] + ' Hundred ' + helper(n % 100)
+        
         res = ''
-        i = 0
+        i = 0 # tracks scale ((1-999), thousand, million,...)
         while num > 0:
-            # skip handles xx, 000, xxx case
+            # skip 000 in nums like xx, 000, xxx
             if num % 1000 != 0:
-                # num % 1000 to get 3 rightmost digits
+                # num % 1000: get 3 rightmost digits
                 res = helper(num % 1000) + scale[i] + ' ' + res
 
-            # num // 1000 to remove 3 rightmost digits
-            num //= 1000
-            i += 1 # trackes scale & increments per 000 chunk
+            num //= 1000 # strip 3 rightmost digits
+            i += 1 # advance scale
 
         return res.strip()
