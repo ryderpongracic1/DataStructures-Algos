ce4b0b8b762dadf2cc0541d81477e33291624dd4
ryder <ryderjpm@gmail.com>
2026-07-19 15:16:02 -0400

    Time: 0 ms (100.00%) | Memory: 19.7 MB (15.64%) - LeetSync

--- 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.py
@@ -2,13 +2,19 @@
 m = (y2 - y1) / (x2 - x1)
 => ((yi - y0) / (xi - x0)) = ((y2 - y1) / (x2 - x1))
 => (y2 - y0) * (x2 - x1) = (y2 - y1) * (xi - x0)
+
+diff between first 2 pts: dx = x1 - x0, dy = y1 - y0
+for pt i, check: dy * (xi - x0) == (yi - y0) * dy
 """
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        x0, y0 = coordinates[0][0], coordinates[0][1]
-        for i in range(1, len(coordinates)):
-            x2, y2 = coordinates[i][0], coordinates[i][1]
-            x1, y1 = coordinates[i - 1][0], coordinates[i - 1][1]
-            if (y2 - y0) * (x2 - x1) != (y2 - y1) * (x2 - x0):
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+        dx = x1 - x0
+        dy = y1 - y0
+
+        for i in range(2, len(coordinates)):
+            xi, yi = coordinates[i]
+            if dy * (xi - x0) != (yi - y0) * dx:
                 return False
         return True
