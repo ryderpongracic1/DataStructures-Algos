35f78af928533f917342879be347fa662bacf436
ryder <ryderjpm@gmail.com>
2026-07-19 23:49:17 -0400

    Time: 2457 ms (21.94%) | Memory: 26.8 MB (11.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,18 +1,37 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def isPalindrome(i, j):
-            while i > -1 and j < len(s) and s[i] == s[j]:
-                i -= 1
-                j += 1
-            # loop breaks when s[i] != s[j] => return last good slice
-            return s[i + 1: j]
+        n = len(s)
+        if n < 2:
+            return s
 
-        res = ''
-        for i in range(len(s)):
-            odd, even = isPalindrome(i, i), isPalindrome(i, i + 1)
+        # dp[i][j] means s[i:j + 1] is a palindromic substring
+        dp = [[False] * n for _ in range(n)]
 
-            if len(odd) > len(res):
-                res = odd
-            if len(even) > len(res):
-                res = even
-        return res
+        # base case: 1 char is palindrome
+        for i in range(n):
+            dp[i][i] = True
+
+        start = 0 # start idx of longest palindromic substring
+        max_len = 1 # length of longest palindromic substring
+
+        # increasing substring lengths from 2 chars to n (entire string)
+        for length in range(2, n + 1):
+
+            # last idx of substring given fixed length
+            for i in range(n - length + 1):
+                j = i + length - 1 # last idx of substring with fixed len
+                if s[i] != s[j]:
+                    dp[i][j] = False
+                
+                # s[i] == s[j]
+                elif length == 2:
+                    dp[i][j] = True
+                else:
+                    # s[i] == s[j],
+                    # is the sub-substring s[i + 1: j] palindromic?
+                    dp[i][j] = dp[i + 1][j - 1]
+
+                # save longest palindromic substring
+                if dp[i][j] and length > max_len:
+                    start, max_len = i, length
+        return s[start:start + max_len]
