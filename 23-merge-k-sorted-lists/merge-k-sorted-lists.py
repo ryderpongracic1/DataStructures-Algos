2a0bab19f9ef672bf0ee8226040f28afb1cacb8e
ryder <ryderjpm@gmail.com>
2026-07-20 03:35:27 -0400

    Time: 6 ms (86.81%) | Memory: 23.1 MB (16.38%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -3,34 +3,27 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
 
-        def mergeTwoLists(node1, node2):
-            head = ListNode(0)
-            curr = head
-            while node1 and node2:
-                if node1.val < node2.val:
-                    curr.next = node1
-                    node1 = node1.next
-                else:
-                    curr.next = node2
-                    node2 = node2.next
-                curr = curr.next
-            if node1:
-                curr.next = node1
-            elif node2:
-                curr.next = node2
-            return head.next
-        
-        while len(lists) > 1:
-            merged = []
-            for i in range(0, len(lists), 2):
-                node1 = lists[i]
-                node2 = None if i + 1 >= len(lists) else lists[i + 1]
-                merged.append(mergeTwoLists(node1, node2))
-            lists = merged
+        heap = [] # val, node
+        counter = 0
+        for node in lists:
+            if node:
+                heapq.heappush(heap, (node.val, counter, node))
+                counter += 1
 
-        return lists[0]
+        head = ListNode(0)
+        curr = head
+        while heap:
+            _, _, node = heapq.heappop(heap)
+            curr.next = node
+            curr = curr.next
+            node = node.next
+            if node:
+                heapq.heappush(heap, (node.val, counter, node))
+                counter += 1
+        return head.next
