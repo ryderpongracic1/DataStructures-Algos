3c9c5ec4783a36bae1ef27748deeb3952543e8ab
ryder <ryderjpm@gmail.com>
2026-07-22 08:37:09 -0400

    Time: 553 ms (43.03%) | Memory: 75.2 MB (24.73%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.py
@@ -1,24 +1,3 @@
-'''
-NOTES
-- All positive weights: Djikstra's finds abs shortest path
-- shortest path will never revisit node - 'at most once' switch is satisfied
-
-APPROACH: Dijkastra's on modified graph
-i) Build adj list
-- for every [u, v, w]:
- - add (v, w) to adj[u]
- - add (u, 2w) to adj[v]
-ii) Init Dijkstra's State:
-- create min_costs init to inf for all nodes
-- create min heap to store (current cost, current node) init with (0, 0)
-iii) Process Heap
-- pop min
-- check if current cost > min_costs[node]: already found better path
-- check if reached target (n - 1)
-- Explore neighbors (adj[node])
- - new_cost = current cost + w
- - if new_cost < min_costs[v]: update & push to heap
-'''
 import heapq
 class Solution:
     def minCost(self, n: int, edges: List[List[int]]) -> int:
@@ -31,11 +10,13 @@ class Solution:
         min_costs[0] = 0
         heap = [(0, 0)]
 
+        # Djikstra's BFS
         while heap:
             cost, node = heapq.heappop(heap)
             if cost > min_costs[node]:
                 continue
             min_costs[node] = cost
+
             if node == n - 1:
                 return cost
             for v, w in adj[node]:
