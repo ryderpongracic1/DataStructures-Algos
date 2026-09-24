98ea8e83b8929e0e9516aa3650fb59db847cf504
ryder <ryderjpm@gmail.com>
2026-09-24 17:00:53 -0400

    Time: 123 ms (85.24%) | Memory: 78.9 MB (44.50%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,89 +1,44 @@
-'''
-- DLL to track LRU key
-- hashmap to track key -> node & hashmap to track frequency -> nodes with that frequency
-'''
-class Node:
-    def __init__(self, key=0, val=0):
-        self.key, self.val = key, val
-        self.next, self.prev = None, None
-        self.freq = 1
-class DLL:
-    def __init__(self):
-        # dummy pts for O(1) access to LRU
-        self.head, self.tail = Node(), Node()
-        self.head.next, self.tail.prev = self.tail, self.head
-        self.size = 0 # how many nodes in DLL for this frequency
-
-    def add_front(self, node):
-        old_ptr = self.head.next
-        self.head.next, node.next = node, old_ptr
-        node.prev, old_ptr.prev = self.head, node
-        self.size += 1
-
-    def remove(self, node):
-        prev_ptr, next_ptr = node.prev, node.next
-        prev_ptr.next, next_ptr.prev = next_ptr, prev_ptr
-        self.size -= 1
-
-    def pop_tail(self):
-        if self.size == 0:
-            return None
-
-        node = self.tail.prev
-        self.remove(node)
-        return node
-        
+from collections import defaultdict, OrderedDict
 class LFUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
+        self.cache = {} # key -> val
+        self.freq = {} # key -> frequency
+        self.buckets = defaultdict(OrderedDict) # freq -> keys
         self.min_freq = 0
-        self.cache = {} # key -> Node
-        self.freq_map = collections.defaultdict(DLL) # freq -> nodes at that freq
-    
-    def _bump(self, node):
-        freq = node.freq
 
-        # self.remove(node)
-        self.freq_map[freq].remove(node)
-        if self.freq_map[freq].size == 0:
-            del self.freq_map[freq]
-
-            # only check min_freq AFTER removing a node from its DLL
-            # & only update min_freq if DLL is empty
-            if self.min_freq == freq:
+    def _bump(self, key):
+        f = self.freq[key]
+        del self.buckets[f][key]
+        if not self.buckets[f]:
+            del self.buckets[f]
+            if self.min_freq == f:
                 self.min_freq += 1
 
-        node.freq += 1
-        self.freq_map[node.freq].add_front(node)
+        self.freq[key] += 1
+        self.buckets[f + 1][key] = None
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
+        self._bump(key)
+        return self.cache[key]        
 
-        node = self.cache[key]
-        self._bump(node)
-        return node.val
-        
     def put(self, key: int, value: int) -> None:
-        if self.capacity == 0:
-            return
-
         if key in self.cache:
-            node = self.cache[key]
-            node.val = value
-
-            self._bump(node)
+            self.cache[key] = value
+            self._bump(key)
             return
 
-        if self.capacity <= len(self.cache):
-            lru = self.freq_map[self.min_freq].pop_tail()
-            del self.cache[lru.key]
-        node = Node(key, value)
-        self.min_freq = 1
-        self.freq_map[1].add_front(node)
-        self.cache[key] = node
+        if len(self.cache) == self.capacity:
+            evict, _ = self.buckets[self.min_freq].popitem(last=False)
+            del self.cache[evict], self.freq[evict]
 
+        self.cache[key] = value
+        self.freq[key] = 1
+        self.min_freq = 1
+        self.buckets[1][key] = None
 
 # Your LFUCache object will be instantiated and called as such:
 # obj = LFUCache(capacity)
